e9bac6eee56a8694eede2bd6ded750ff21d7c7f7
butibuti <butikouki@gmail.com>
2021-01-21 11:04:22 +0900

    add editor component

--- ButiEngine_User/MapEditorComponent.cpp
+++ ButiEngine_User/MapEditorComponent.cpp
@@ -1,9 +1,29 @@
 #include "stdafx_u.h"
 #include "MapEditorComponent.h"
 #include "MapComponent.h"
+#include"GameSettings.h"
 
 void ButiEngine::MapEditorComponent::OnUpdate()
 {
+    static int blockPos[3];
+    static int invisibleID;
+
+    GUI::Begin("Editor");
+
+    GUI::BulletText("New Block Position");
+    GUI::DragInt3("##position", blockPos, 1.0, 0, 300);
+
+    if (GUI::Button("Add Block")) {
+        shp_currentMapData->mapData[blockPos[0]][blockPos[1]][blockPos[2]] = GameSettings::block;
+    }
+    if (GUI::Button("Add Goal")) {
+        shp_currentMapData->mapData[blockPos[0]][blockPos[1]][blockPos[2]] = GameSettings::defaultGoal;
+    }
+    if (GUI::Button("Add InvisibleBlock")) {
+        shp_currentMapData->mapData[blockPos[0]][blockPos[1]][blockPos[2]] = GameSettings::block;
+    }
+
+    GUI::End();
 }
 
 void ButiEngine::MapEditorComponent::OnSet()
