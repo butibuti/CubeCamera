6fa79e2be95b265cd91794102f7c91cad245633f
butibuti <butikouki@gmail.com>
2021-01-22 00:21:36 +0900

    Add Stage Edit System

--- Resources/Shader/ImmediateParticleVS.hlsl
+++
@@ -1,7 +0,0 @@
-#include"DefaultShader.hlsli"
-
-Vertex_UV_Normal_Color VSMain(Vertex_UV_Normal_Color vertex)
-{
-	vertex.position = mul(vertex.position, modelMatrix);
-	return vertex;
-}
--- Resources/Shader/VertexUVNormalAttributeVS.hlsl
+++
@@ -1,8 +0,0 @@
-#include"DefaultShader.hlsli"
-
-Vertex_UV_Normal VSMain(Vertex_UV_Normal vertex)
-{
-	vertex.position = mul(vertex.position, modelMatrix);
-	vertex.normal = mul(vertex.normal, (float3x3)modelMatrix);
-	return vertex;
-}
