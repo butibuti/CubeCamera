e217deabad83e4e1fac4cf32fb47bb97e1db6298
butibuti <butikouki@gmail.com>
2021-01-28 14:20:07 +0900

    fix

--- ButiEngine_User/GameSettings.h
+++ ButiEngine_User/GameSettings.h
@@ -5,12 +5,15 @@ class GameSettings
 public:
 	~GameSettings(){}
 	static float BlockSize;
-	static int player;
-	static int block;
-	static int tutorialGoal;
-	static int easyGoal;
-	static int defaultGoal;
-	static int invisibleBlock;
+	static const int player=1;
+	static const int block=2;
+	static const int tutorialGoal=3;
+	static const int easyGoal=4;
+	static const int defaultGoal=5;
+	static const int playerRotate_90 = 6;
+	static const int playerRotate_180 = 7;
+	static const int playerRotate_min90 = 8;
+	static const int invisibleBlock=100;
 
 	static bool CheckRight();
 	static bool CheckLeft();
