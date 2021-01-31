0c85a5932f33f6a1ef96944f6978e8f81b18ba38
butibuti <butikouki@gmail.com>
2021-02-01 00:31:47 +0900

    fix noshadeShader

--- ButiEngine_User/Resources/Shader/DefaultMeshPS_noShade.hlsl
+++ ButiEngine_User/Resources/Shader/DefaultMeshPS_noShade.hlsl
@@ -4,5 +4,5 @@ float4 PSMain(Pixel_UV_Normal pixel) : SV_TARGET
 	float4 Tex = (mainTexture.Sample(mainSampler,pixel.uv).rgba);
 	
 	
-	return  float4((Tex).rgb, Tex.a);
+	return  float4((Tex).rgb*lightDir.rgb, Tex.a);
 }
